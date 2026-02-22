5ffc1a83fef886c1ad02c473c36ef87a6162e376
vhbnguyen <156697470+vhbnguyen@users.noreply.github.com>
2026-02-22 03:22:21 +0000

    rerouted nav bar to groups

--- client/src/components/App/Navbar.js
+++ client/src/components/App/Navbar.js
@@ -1,43 +1,87 @@
+// import React from "react";
+// import {AppBar, Toolbar, Typography, Button, Grid
+// } from "@mui/material";
+
+// function Navbar() {
+//   return (
+//     <AppBar position="static">
+//       <Toolbar>
+//         <Grid container alignItems="center">
+          
+//           <Grid item xs={6}>
+//             <Typography variant="h6">
+//               UW Connect
+//             </Typography>
+//           </Grid>
+
+//           {/* Right side - Navigation Buttons */}
+//           <Grid item xs={6} container justifyContent="flex-end" spacing={2}>
+//             <Grid item>
+//               <Button color="inherit">
+//                 Home
+//               </Button>
+//             </Grid>
+//             <Grid item>
+//               <Button color="inherit">
+//                 Groups
+//               </Button>
+//             </Grid>
+//             <Grid item>
+//               <Button color="inherit">
+//                 Profile
+//               </Button>
+//             </Grid>
+//             <Grid item>
+//               <Button color="inherit">
+//                 Events
+//               </Button>
+//             </Grid>
+//           </Grid>
+
+//         </Grid>
+//       </Toolbar>
+//     </AppBar>
+//   );
+// }
+
+// export default Navbar;
+
+// client/src/components/App/Navbar.js
 import React from "react";
-import {AppBar, Toolbar, Typography, Button, Grid
-} from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, Grid } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Grid container alignItems="center">
-          
           <Grid item xs={6}>
-            <Typography variant="h6">
-              UW Connect
-            </Typography>
+            <Typography variant="h6">UW Connect</Typography>
           </Grid>
 
-          {/* Right side - Navigation Buttons */}
           <Grid item xs={6} container justifyContent="flex-end" spacing={2}>
             <Grid item>
-              <Button color="inherit">
+              <Button color="inherit" component={RouterLink} to="/home">
                 Home
               </Button>
             </Grid>
             <Grid item>
-              <Button color="inherit">
+              <Button color="inherit" component={RouterLink} to="/groups">
                 Groups
               </Button>
             </Grid>
             <Grid item>
-              <Button color="inherit">
+              <Button color="inherit" component={RouterLink} to="/profile">
                 Profile
               </Button>
             </Grid>
             <Grid item>
-              <Button color="inherit">
+              <Button color="inherit" component={RouterLink} to="/events">
                 Events
               </Button>
             </Grid>
           </Grid>
-
         </Grid>
       </Toolbar>
     </AppBar>
--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -19,26 +19,63 @@
 
 // export default App;
 
+// import * as React from "react";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// import GroupsPage from "./GroupsPage";
+// import CreateGroupForm from "./CreateGroupForm";
+// import GroupDetailsPage from "./GroupDetailsPage";
+// import EditGroupForm from "./EditGroupForm";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Navigate to="/groups" replace />} />
+//         <Route path="/groups" element={<GroupsPage />} />
+//         <Route path="/groups/new" element={<CreateGroupForm />} />
+//         <Route path="/groups/:groupId" element={<GroupDetailsPage />} />
+//         <Route path="/groups/:groupId/edit" element={<EditGroupForm />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+// client/src/components/App/index.js
 import * as React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Layout from "./Layout";
 import GroupsPage from "./GroupsPage";
 import CreateGroupForm from "./CreateGroupForm";
 import GroupDetailsPage from "./GroupDetailsPage";
 import EditGroupForm from "./EditGroupForm";
 
-const App = () => {
+// placeholders (optional)
+const HomePage = () => <div>Home</div>;
+const ProfilePage = () => <div>Profile</div>;
+const EventsPage = () => <div>Events</div>;
+
+export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/groups" replace />} />
-        <Route path="/groups" element={<GroupsPage />} />
-        <Route path="/groups/new" element={<CreateGroupForm />} />
-        <Route path="/groups/:groupId" element={<GroupDetailsPage />} />
-        <Route path="/groups/:groupId/edit" element={<EditGroupForm />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<HomePage />} />
+
+          {/* ✅ Groups routes */}
+          <Route path="/groups" element={<GroupsPage />} />
+          <Route path="/groups/new" element={<CreateGroupForm />} />
+          <Route path="/groups/:groupId" element={<GroupDetailsPage />} />
+          <Route path="/groups/:groupId/edit" element={<EditGroupForm />} />
+
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/events" element={<EventsPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
-};
-
-export default App;
+}
