6d2846978241c54e256d5e51cdc30fe068273c53
b22stewa <156976294+b22stewa@users.noreply.github.com>
2026-02-27 04:11:23 +0000

    downloaded multer

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
-import theme from "./components/App/theme";
+import theme from "./components/App/Theme";
 import App from "./components/App";
 
 const root = createRoot(document.getElementById("root"));
