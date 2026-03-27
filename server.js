a2394563845eba81ddbefeb21b9b72562e67793a
PallaviHooda <156486917+PallaviHooda@users.noreply.github.com>
2026-03-27 03:38:12 +0000

    fixed an error with images not showing on posts

--- server.js
+++ server.js
@@ -3592,6 +3592,7 @@ app.post('/api/posts', checkAuth, upload.single('image'), async (req, res) => {
                 author_id: author_id,
                 title,
                 description: content,
+                image_url,
                 tags: tagResult.map(t => t.tag_name),
                 createdAt: new Date().toISOString(),
                 like_count: 0,
@@ -3603,7 +3604,7 @@ app.post('/api/posts', checkAuth, upload.single('image'), async (req, res) => {
         } else {
           //db.end();
           res.json({
-            post: { post_id: postId, author_id: author_id, title, description: content, tags: [], createdAt: new Date().toISOString(), like_count: 0, liked_by_me: false },
+            post: { post_id: postId, author_id: author_id, title, description: content, image_url, tags: [], createdAt: new Date().toISOString(), like_count: 0, liked_by_me: false },
             message: 'Post created successfully but no valid tags found'
           });
         }
@@ -3611,7 +3612,7 @@ app.post('/api/posts', checkAuth, upload.single('image'), async (req, res) => {
     } else {
       //db.end();
       res.json({
-        post: { post_id: postId, author_id: author_id, title, description: content, tags: [], createdAt: new Date().toISOString(), like_count: 0, liked_by_me: false },
+        post: { post_id: postId, author_id: author_id, title, description: content, image_url, tags: [], createdAt: new Date().toISOString(), like_count: 0, liked_by_me: false },
         message: 'Post created successfully without tags'
       });
     }
